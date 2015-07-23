627bbf2599c0a08df5d0c709bd1a689c9beafe42
Johnson Wang <johnsonwangnz@gmail.com>
2015-07-23 14:22:59 +1200

    Setup of solution

---
+++ MyContract/EchoServices/EchoService.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+using System.ServiceModel;
+using MyContract;
+
+namespace EchoServices
+{
+    [ServiceBehavior(Name = "EchoService", Namespace = "http://samples.microsoft.com/ServiceModel/Relay/")]
+    public class EchoService : IEchoContract
+    {
+        public string Echo(string text)
+        {
+            Console.WriteLine("Echoing: {0}", text);
+            return text;
+        }
+    }
+}
